dc7e7c8482ebd6bbffb6002701818a074178127f
maniram3112 <maniramthota12@gmail.com>
2023-03-25 12:04:39 +0530

    basic version

---
+++ admin/index.php
@@ -0,0 +1,3 @@
+<?php include('partials/menu.php');?>
+<?php include('partials/main-content.php');?>
+<?php include('partials/footer.php');?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer Section starts -->
+<div class = "footer">
+        <div class = "wrapper">
+            <p class = "text-center">2023 All rights reserved , Night Canteen , Developed By -<a href= "#"> Rajan Gupta</a></p>
+        </div>
+    </div>
+    <!-- Footer Section ends -->
+
+</body>
